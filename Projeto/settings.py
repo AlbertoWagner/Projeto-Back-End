0ec639f7d36b95c20f078d93e47f534cddaf0201
alberto <alberto.oliveira@ccc.ufcg.edu.br>
2020-12-19 18:17:29 -0300

    add Django admin

--- Projeto/settings.py
+++ Projeto/settings.py
@@ -13,7 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'Projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
